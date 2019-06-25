cebe757b6115b0aa97a3c5baa2e00ecb27392ab9
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-06-25 18:43:37 +0900

    Change license to BSD-2-Clause

--- src/mc_vrep_cli.cpp
+++ src/mc_vrep_cli.cpp
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
 #include "mc_vrep_cli.h"
 
 #include <mc_rtc/logging.h>
--- src/vrep_simulation.cpp
+++ src/vrep_simulation.cpp
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
 #include "vrep_simulation.h"
 
 #include <vrep-api-wrapper/vrep.h>
